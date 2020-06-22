5cca73c044ff89c687239e56be280f8d6350db46
John Doan <johndoan42@gmail.com>
2020-06-22 20:35:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,5 @@
 def reverse_each_word(string)
-  sentence = ''
-  string.split().each do |word|
-  sentence += word.reverse+" "
+  string.split().collect do |word|
+    word.reverse+" "
   end
-  sentence[0...-1]
 end
